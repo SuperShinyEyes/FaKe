7686957e08ddbf3e4f097606f52cb04ed303f563
Young Park <i.make.seyoung@gmail.com>
2015-12-23 13:36:18 +0900

    Change /static/ => /assets/

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -143,7 +143,7 @@ if "DYNO" in os.environ:
 else:
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/1.8/howto/static-files/
-    STATIC_PATH = os.path.join(BASE_DIR, 'static/')
+    STATIC_PATH = os.path.join(BASE_DIR, 'assets/')
 
     STATICFILES_DIRS = (
       STATIC_PATH,
